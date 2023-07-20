ed6ed0eff78a01273eb7905159a8dc8467c5612e
yegorov <evgenyyegorov@gmail.com>
2023-07-20 18:14:09 +0400

    Filter was added

--- main.tf
+++ main.tf
@@ -6,10 +6,31 @@ resource "aws_route_table" "rt" {
   }
 }
 
-resource "aws_route_table_association" "rt_association" {
-  count          = var.subnet_id || var.gateway_id ? 1 : 0
-  route_table_id = aws_route_table.rt.id
-  subnet_id      = var.subnet_id
-  gateway_id     = var.gateway_id
+data "aws_subnets" "subnet" {
+  count = var.subnet_name != null ? 1 : 0
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [var.subnet_name]
+  }
+
+  filter {
+    name   = "availabilityZone"
+    values = [var.subnet_zone]
+  }
 }
 
+locals {
+  subnet_id = var.subnet_id != null ? var.subnet_id : data.aws_subnets.subnet[0].ids[0]
+}
+
+resource "aws_route_table_association" "rt_association" {
+  count          = local.subnet_id != null || var.gateway_id != null ? 1 : 0
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = local.subnet_id
+  gateway_id     = var.gateway_id
+}
