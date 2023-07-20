90f9db26dd25dca0572a1613dc9dd4db65128358
yegorov <evgenyyegorov@gmail.com>
2023-07-20 14:57:02 +0400

    Init module

---
+++ output.tf
@@ -0,0 +1,4 @@
+output "subnets" {
+  description = "Route table"
+  value       = aws_route_table.rt
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+  nullable    = false
+}
+
+variable "name" {
+  description = "Route table name"
+  type        = string
+  nullable    = false
+}
